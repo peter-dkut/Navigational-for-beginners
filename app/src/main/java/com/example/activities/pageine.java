d571e844f244e18d4d0940a41049433e6d709c61
peter-dkut <peter.mwaura18@students.dkut.ac.ke>
2020-11-18 17:03:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/activities/pageine.java
@@ -0,0 +1,16 @@
+package com.example.activities;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class pageine extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstances)
+    {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.pageine);
+    }
+}
